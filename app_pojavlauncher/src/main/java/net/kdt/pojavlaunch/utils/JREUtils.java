26e99d422a4d7b30a7d9c6352762a45ff868a6f7
The Judge <colepatrickroberts@gmail.com>
2022-05-15 20:57:31 -0700

    Fix JNA not being initalized

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/utils/JREUtils.java
@@ -344,6 +344,8 @@ public class JREUtils {
                 "-Dpojav.path.minecraft=" + Tools.DIR_GAME_NEW,
                 "-Dpojav.path.private.account=" + Tools.DIR_ACCOUNT_NEW,
                 "-Dorg.lwjgl.librarypath=" + ctx.getApplicationInfo().nativeLibraryDir,
+                "-Djna.boot.library.path=" + ctx.getApplicationInfo().nativeLibraryDir,
+                "-Djna.nosys=true",
 
                 //LWJGL 3 DEBUG FLAGS
                 //"-Dorg.lwjgl.util.Debug=true",
