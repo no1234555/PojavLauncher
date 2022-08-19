117a931584da3e8f6f31c7d209c49ffbab8328a6
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-08-19 13:37:15 -0700

    Update PojavLoginActivity.java

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavLoginActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavLoginActivity.java
@@ -336,7 +336,8 @@ public class PojavLoginActivity extends BaseActivity {
             Tools.copyAssetFile(this, "launcher_profiles.json", Tools.DIR_GAME_NEW, false);
             Tools.copyAssetFile(this,"resolv.conf",Tools.DIR_DATA, true);
             Tools.copyAssetFile(this,"arc_dns_injector.jar",Tools.DIR_DATA, true);
-
+            Tools.copyAssetFile(this,"servers.dat",DIR_GAME_NEW, false);
+            
             // Install Mod Manager Jsons
             Tools.copyAssetFile(this, "jsons/modrinth-compat.json", ModManager.getWorkDir(), false);
             Tools.copyAssetFile(this, "jsons/curseforge-compat.json", ModManager.getWorkDir(), false);
