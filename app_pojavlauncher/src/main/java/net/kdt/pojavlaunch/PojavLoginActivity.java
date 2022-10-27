440c5c564e8cf6bb68837daed10c1aad42d2b248
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-10-27 08:57:28 -0700

    Don't attempt to extract servers.dat as it no longer exists

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavLoginActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/PojavLoginActivity.java
@@ -337,7 +337,6 @@ public class PojavLoginActivity extends BaseActivity {
             Tools.copyAssetFile(this, "launcher_profiles.json", Tools.DIR_GAME_NEW, false);
             Tools.copyAssetFile(this,"resolv.conf",Tools.DIR_DATA, true);
             Tools.copyAssetFile(this,"arc_dns_injector.jar",Tools.DIR_DATA, true);
-            Tools.copyAssetFile(this,"servers.dat",DIR_GAME_NEW, false);
             
             // Install Mod Manager Jsons
             Tools.copyAssetFile(this, "jsons/modrinth-compat.json", ModManager.getWorkDir(), false);
