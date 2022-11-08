d5f74d40d2a0610a0e872225da304f42a55c5e16
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-11-08 13:14:01 -0800

    Test

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/MainActivity.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/MainActivity.java
@@ -3,6 +3,7 @@ package net.kdt.pojavlaunch;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import net.kdt.pojavlaunch.customcontrols.ControlLayout;
 import net.kdt.pojavlaunch.prefs.LauncherPreferences;
@@ -19,6 +20,19 @@ public class MainActivity extends BaseMainActivity {
         MCXRLoader.setActivity(this);
         MCXRLoader.launch(this);
         super.onCreate(savedInstanceState);
+        setFullScreen();
+    }
+
+    protected void setFullScreen() {
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+
+        getWindow().getDecorView().setSystemUiVisibility(flags);
     }
 
     @Override
